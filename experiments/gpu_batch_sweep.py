d318448b3c4cf767eb37c35b35e8318c8c8fe7a4
Abhinav Kumar <abhinav333.ak@outlook.com>
2025-05-02 21:30:22 -0400

    config changes

--- experiments/gpu_batch_sweep.py
+++ experiments/gpu_batch_sweep.py
@@ -3,7 +3,7 @@ from Utils.plotting import plot_batchsize_sweep
 
 
 def batch_size_sweep_gpu(subset=False, dataset_size=5000, epochs=10, learning_rate=0.0005):
-    batch_sizes = [16, 32, 64, 128]
+    batch_sizes = [8, 16, 32, 64, 128]
     models = ["resnet18", "mobilenetv2"]
     results = []
 
